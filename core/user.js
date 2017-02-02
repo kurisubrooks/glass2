7550e3a35446e9df8eae394e2b8183ea624353fc
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-02 18:30:41 +1100

    Add WIP Notification, User classes

---
+++ core/user.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt-nodejs");
+const fs = require("fs");
+
+module.exports = class User {
+    constructor() {
+        return null;
+    }
+
+    check() {
+        return null;
+    }
+
+    new() {
+        return null;
+    }
+
+    delete() {
+        return null;
+    }
+};
