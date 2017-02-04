ae5e607835404cf9a49ef0863a7d8f9668997371
Kurisu Brooks <kurisubrooks@gmail.com>
2017-02-04 13:19:45 +1100

    Set Window Colour to Black

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ let startup = () => {
         width: 1280,
         minWidth: 1024,
         height: 720,
-        minHeight: 576
+        minHeight: 576,
+        backgroundColor: "#000000"
     });
 
     win.loadURL(`file://${__dirname}/render/startup.html`);
