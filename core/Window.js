b09968eb32b23967c471c65874ee49ee9f98abd9
craigmc08 <craigmc08@gmail.com>
2017-04-18 05:51:54 -0400

    Window Resizing and Moving

--- core/Window.js
+++ core/Window.js
@@ -29,6 +29,15 @@ class Window {
             height: this.size[1]
         });
 
+        // Initialize events for dragging window around
+        this.window.find('.window-title').mousedown((e) => { this.startDrag(e.pageX, e.pageY); });
+
+        // Initialize events for resizing window
+        this.window.find('.window-resize').mousedown((e) => { this.startResize(e.pageX, e.pageY); });
+
+        $(document).mousemove((e) => { this.doMousemovement(e.pageX, e.pageY); });
+        $(document).mouseup((e) => { this.stopMovements(); });
+
         if (this.theme === "dark") this.window.addClass("dark");
     }
 
@@ -58,13 +67,22 @@ class Window {
 
     maximize() {
         if (!this.maximized) {
-            this.window.width(this.window.parent().width());
-            this.window.height(this.window.parent().height());
+            this.oldX = this.posX;
+            this.oldY = this.posY;
+            this.posX = 0;
+            this.posY = 0;
+            this.size = [this.width, this.height];
+
+            this.width = this.window.parent().width();
+            this.height = this.window.parent().height();
 
             this.window.addClass("maximized");
         } else {
-            this.window.width(this.size[0]);
-            this.window.height(this.size[1]);
+            this.width = this.size[0];
+            this.height = this.size[1];
+
+            this.posX = this.oldX;
+            this.posY = this.oldY;
 
             this.window.removeClass("maximized");
         }
@@ -74,6 +92,68 @@ class Window {
 
     close() {
         this.window.remove();
+    }
+
+    startDrag(mouseX, mouseY) {
+      if (this.maximized) {
+        this.maximize();
+        this.posX = 0;
+        this.posY = 0;
+      }
+      this.dragging = true;
+      this.startMovement(mouseX, mouseY);
+    }
+
+    startResize(mouseX, mouseY) {
+      this.resizing = true;
+      this.startMovement(mouseX, mouseY);
+    }
+
+    startMovement(mouseX, mouseY) {
+      this.mx = mouseX;
+      this.my = mouseY;
+    }
+    doMousemovement(mouseX, mouseY) {
+      let dx = mouseX - this.mx, dy = mouseY - this.my;
+      this.mx = mouseX;
+      this.my = mouseY;
+      if (this.dragging) {
+        this.posX += dx;
+        this.posY += dy;
+      }
+      if (this.resizing) {
+        this.width += dx;
+        this.height += dy;
+      }
+    }
+    stopMovements () {
+      this.resizing = this.dragging = false;
+    }
+
+    get posX () {
+      return parseInt(this.window.css('left'));
+    }
+    set posX (val) {
+      this.window.css('left', Math.max(val, 0));
+    }
+    get posY () {
+      return parseInt(this.window.css('top'));
+    }
+    set posY (val) {
+      this.window.css('top', Math.max(val, 0));
+    }
+
+    get width () {
+      return parseInt(this.window.css('width'));
+    }
+    set width (val) {
+      this.window.css('width', Math.max(val, 0));
+    }
+    get height () {
+      return parseInt(this.window.css('height'));
+    }
+    set height (val) {
+      this.window.css('height', Math.max(val, 0));
     }
 }
 
