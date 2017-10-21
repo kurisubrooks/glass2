4ce871bbf744c0ad51755292c2f299de5b3de464
Kurisu Brooks <kurisubrooks@gmail.com>
2017-10-22 05:57:43 +1100

    Clear Password Field when switching user, cleanup

---
+++ render/scripts/login.js
@@ -0,0 +1,84 @@
+const moment = require("moment");
+const $ = window.jQuery = require("jquery");
+
+const users = [
+    {
+        name: "Kurisu Brooks",
+        avatar: "kurisu",
+        password: "cheese"
+    },
+    {
+        name: "Katie Sekai",
+        avatar: "katie",
+        password: "tomato"
+    },
+    {
+        name: "Sammy Sekai",
+        avatar: "sammy",
+        password: "pumpkin"
+    }
+];
+
+let selectedUserIndex = null;
+let selectedUser = null;
+
+const selectUser = head => {
+    head = $(head);
+    $("#password").val("");
+    const index = head.data("user");
+
+    if (selectedUserIndex === index) return;
+
+    $(".head").removeClass("active");
+
+    head.addClass("active");
+    $(".login .form .title").text(users[index].name);
+    selectedUserIndex = index;
+    selectedUser = users[selectedUserIndex];
+
+    $(".ghost").remove();
+
+    const rightElements = users.length - selectedUserIndex - 1;
+
+    addGhosts(Math.abs(selectedUserIndex - rightElements), selectedUserIndex > rightElements);
+};
+
+const addGhosts = (amount, append) => {
+    const type = append ? "append" : "prepend";
+    for (let ghosts = 0; ghosts < amount; ghosts++) {
+        $(".heads")[type]($(`<div class="head ghost"></div>`));
+    }
+};
+
+$(() => {
+    for (let index = 0; index < users.length; index++) {
+        const head = $(`<div class="head" data-user=${index} style="background-image: url(../assets/avatars/${users[index].avatar}.png);"></div>`);
+
+        if (index === 0) selectUser(head);
+
+        $(".heads").append(head);
+    }
+
+    setInterval(() => {
+        let time = moment().format("h:mm");
+        if ($(".time").text() !== time) $(".time").text(time);
+    }, 150);
+
+    $(".head").not(".ghost").click(el => selectUser(el.target));
+
+    $("body").fadeIn(500);
+
+    /*
+    $("login").submit((e) => {
+        let success = true;
+        let user = e.target[0].value.replace("user:", "");
+        let pass = e.target[1].value;
+
+        if (success) {
+            setTimeout(() => $("login").fadeOut(250), 1000);
+            setTimeout(() => $("main").fadeOut(250), 1500);
+            setTimeout(() => remote.getCurrentWindow().loadURL(`file://${__dirname}/desktop.html`), 1600);
+        }
+    });
+    */
+});
